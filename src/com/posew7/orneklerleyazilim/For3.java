ddf4934f2bf99553739ed99b9dd95e443b27a9f2
Yunus <Yunus@DESKTOP-F8LKHQI>
2018-06-23 15:25:47 +0300

    50den 70e kadar olan tek sayıları yazdıran program

---
+++ src/com/posew7/orneklerleyazilim/For3.java
@@ -0,0 +1,14 @@
+package com.posew7.orneklerleyazilim;
+
+public class For3 {
+
+    public static void main(String[] args) {
+
+        for (int i = 51; i < 70; i++) {
+
+            System.out.println(i++ + " ");
+
+        }
+    }
+
+}
