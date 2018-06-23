ba0e2406c4d40a98fcfd9917188166a1eb569a32
Yunus <Yunus@DESKTOP-F8LKHQI>
2018-06-23 15:17:56 +0300

    ekrana yan yana 1den 10a kadar yazdıran program

---
+++ src/com/posew7/orneklerleyazilim/For2.java
@@ -0,0 +1,13 @@
+package com.posew7.orneklerleyazilim;
+
+public class For2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i + " ");
+
+        }
+
+    }
+}
