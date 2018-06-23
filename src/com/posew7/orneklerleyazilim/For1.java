a0844c791ea37927eefb491d614e5fd05820ed38
Yunus <Yunus@DESKTOP-F8LKHQI>
2018-06-23 15:14:59 +0300

    ekrana alt alta 10 defa merhaba yazan program

---
+++ src/com/posew7/orneklerleyazilim/For1.java
@@ -0,0 +1,15 @@
+package com.posew7.orneklerleyazilim;
+
+public class For1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 11; i++) {
+
+            System.out.println(i + " merhaba dünya");
+
+        }
+
+    }
+
+}
